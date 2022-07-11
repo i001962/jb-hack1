bc9f997884db798ecc1b36231dafb9f4d9c5eda7
noctisatrae <pro.hector.kub@gmail.com>
2022-07-11 10:08:03 +0200

    Convlist is to be ignored. Not yet ready. Tho, you can see changes in Chat.jsx

--- packages/react-app/src/views/Chat.jsx
+++ packages/react-app/src/views/Chat.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useCallback, memo } from "react";
 import { Card, Input, Button } from "antd";
-import Account from "../components/Account";
+import List from "../components/Convlist";
 import Gun from "gun";
 
-const gun = Gun();
+const gun = Gun({radisk: false, localStorage:false});
 
 const get =  key => {
      return new Promise((resolve, reject) => {
@@ -16,36 +16,49 @@ const lastOf = async key => {
 }
 
 // push to array last element on change of other  var.
-export default function Chat() {
+export default function Chat({ isLogged }) {
     
     const [msg, setMsg] = useState(["<b>Welcome to this amazing discussion...</b>", "Please feel free to enjoy ;)"]);
+    const [connected, setConnected] = useState(false);
     const [write, setWritten] = useState("");
 
     useEffect(() => {
-        window.localStorage.clear();
+        if (!(isLogged == "")) setConnected(true)
+        else if (isLogged == "") setConnected(false);
+    }, [isLogged])
+
+    useEffect(() => {
         gun.get("chat").map().once(data => {
             setMsg((prev) => ( [...prev, data] ))
         });
     }, [setMsg])
-    return <div id="chat" className="chat">
-        <Account></Account>
-        <Card>
+    return (
+        connected ?         
+        <div id="chat" className="chat">
+            <Card>
+                <h1>Hi {isLogged}!</h1>
+                <List who={isLogged} db={gun}></List>
+            </Card>
+        </div>
 
-            <button onclick="window.localStorage.clear();">Clear All</button>
+        :
 
-            <p>Test</p>
-
-            {msg.map(data =>
-                <li>{data}</li>
-            )}
-
-            <Input placeholder="Type here..." type="text" value={write} onInput={(e) => setWritten(e.target.value)}/>
-            <Button onClick={() => { 
-                console.log(write)
-                gun.get("chat").set(write);
-            }}>Send</Button>
-        </Card>
-    </div>
+        <div>
+            Not Connected
+        </div>
+    )
 }
 
 export const ChatMemo = memo(Chat);
+
+
+
+// {msg.map(data =>
+//     <li>{data}</li>
+// )}
+
+// <Input placeholder="Type here..." type="text" value={write} onInput={(e) => setWritten(e.target.value)}/>
+// <Button onClick={() => { 
+//     console.log(write)
+//     gun.get("chat").set(write);
+// }}>Send</Button>
